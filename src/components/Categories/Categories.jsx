96d4d77e65715b50fb35bef679c78ada02422f66
iyiu9akhan <iyiu9akhan@gmail.com>
2025-12-12 03:11:10 +0600

    windows: recheck

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,19 +3,28 @@ import Container from '../Container/Container'
 import trending_icon from '../../assets/trending/trending_icon.png'
 import { BsArrowRightSquareFill } from "react-icons/bs";
 import categoriesData, { categoriesSlider } from "../Categories/CategoriesData";
-
 import { IoArrowRedoSharp } from "react-icons/io5";
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "../ui/carousel"
-import Autoplay from "embla-carousel-autoplay";
-import { FaStar, FaStarHalfAlt, FaRegStar, FaCartArrowDown } from "react-icons/fa";
+// import {
+//     Carousel,
+//     CarouselContent,
+//     CarouselItem,
+//     CarouselNext,
+//     CarouselPrevious,
+// } from "../ui/carousel"
+// import Autoplay from "embla-carousel-autoplay";
+// import { FaStar, FaStarHalfAlt, FaRegStar, FaCartArrowDown } from "react-icons/fa";
+import Countdown from 'react-countdown';
+import categories_shape_01 from "../../assets/categories/categories_shape_01.png"
+import categories_shape_02 from "../../assets/categories/categories_shape_02.png"
 
 function Categories() {
+
+    // const targetDate = Date.now() + 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
+    const targetDate = localStorage.getItem('countdownEnd') || (() => {
+        const date = Date.now() + 30 * 24 * 60 * 60;
+        localStorage.setItem('countdownEnd', date);
+    })();
+
     return (
         <div className='my-[50px]'>
             <Container>
@@ -26,8 +35,8 @@ function Categories() {
                             <p className='font-heading text-[36px] capitalize font-bold'>browse by categories</p>
                         </div>
                         <div className='flex items-center gap-3 text-[20px] justify-between cursor-pointer group'>
-                            <p className='capitalize font-heading font-semibold text-[15px]  text-red-btn group-hover:text-button duration-300'>view all products</p>
-                            <BsArrowRightSquareFill className='text-red-btn group-hover:text-button duration-300' />
+                            <p className='capitalize font-heading font-semibold text-[15px]  text-orange-btn group-hover:text-button duration-300'>view all products</p>
+                            <BsArrowRightSquareFill className='text-orange-btn group-hover:text-button duration-300' />
                         </div>
                     </div>
                     <div className='flex justify-between  gap-6'>
@@ -43,8 +52,10 @@ function Categories() {
                                 </div>
                             ))}
                         </div>
-                        <div className='w-4/12'>
-                            <Carousel
+
+                        <div className='w-4/12 bg-categoriesCounterBG rounded-[20px] flex flex-col items-center  relative'>
+
+                            {/* <Carousel 
                                 className="rounded-[20px] h-full"
                                 plugins={[
                                     Autoplay({
@@ -71,7 +82,35 @@ function Categories() {
                                     ))}
                                 </CarouselContent>
                                 <p className='font-subHeading capitalize text-orange-btn tracking-widest text-[27px] text-center mt-4'><span className='text-black text-[30px]'>"</span>your next obsession starts here<span className='text-black text-[30px]'>"</span></p>
-                            </Carousel>
+                            </Carousel> */}
+
+                            <p className='text-[50px] capitalize font-heading font-bold text-white text-center leading-13 mt-10 mb-5'>something new just landed</p>
+                            <p className='mb-5 text-white font-bold font-primary text-[20px]'>Hurry up ! Deal end in :</p>
+                            <Countdown
+                                date={parseInt(targetDate)}
+                                renderer={({ days, hours, minutes, seconds }) => (
+                                    <div className='flex font-heading text-white font-bold text-[25px] text-center gap-3'>
+                                        <div>
+                                            <p className='w-[55px] h-[55px] bg-button rounded-[15px] flex items-center justify-center -mb-[7px]'>{days}</p>
+                                            <span className='text-[14px]'>Days</span>
+                                        </div>
+                                        <div>
+                                            <p className='w-[55px] h-[55px] bg-button rounded-[15px] flex items-center justify-center -mb-[7px]'>{hours}</p>
+                                            <span className='text-[14px]'>Hrs</span>
+                                        </div>
+                                        <div>
+                                            <p className='w-[55px] h-[55px] bg-button rounded-[15px] flex items-center justify-center -mb-[7px]'>{minutes}</p>
+                                            <span className='text-[14px]'>Mins</span>
+                                        </div>
+                                        <div>
+                                            <p className='w-[55px] h-[55px] bg-button rounded-[15px] flex items-center justify-center -mb-[7px]'>{seconds}</p>
+                                            <span className='text-[14px]'>Secs</span>
+                                        </div>
+                                    </div>
+                                )}
+                            />
+                            <img src={categories_shape_01} alt="categories_shape_01" className='absolute opacity-30 -bottom-8' />
+                            <img src={categories_shape_02} alt="categories_shape_02" className='absolute bottom-2 h-50 w-60 left-35' />
                         </div>
                     </div>
                 </div>

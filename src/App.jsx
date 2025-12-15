ec02bfc9ead254691d61eaf32ce0734edc1c36d5
iyiu9akhan <iyiu9akhan@gmail.com>
2025-12-15 20:00:26 +0600

    windows: megaMenu

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import Trending from './components/Trending/Trending'
 import Offer from './components/Offer/Offer'
 import Categories from './components/Categories/Categories'
 import ImgProduct from './components/ImgProduct/ImgProduct'
-import Menu_Shop from './components/Menubar/MenuPages/Menu_Shop'
+import MegaMenu from './components/Menubar/MegaMenu'
 
 function App() {
 
   return (
     <div className='h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-button scrollbar-track-gray-100'>
       <Navbar />
-      <Menubar />
+      {/* <Menubar /> */}
+      <MegaMenu/>
       <Banner />
       <Categories />
       <Trending />
